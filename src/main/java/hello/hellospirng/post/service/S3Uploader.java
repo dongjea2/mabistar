d1cdbb1d8a767cc4bd09f247c1f5c7376733221d
dongjea2 <dewawd@gmail.com>
2022-03-31 01:47:26 +0900

    S3Uploader.java delete메소드 추가

--- src/main/java/hello/hellospirng/post/service/S3Uploader.java
+++ src/main/java/hello/hellospirng/post/service/S3Uploader.java
@@ -2,6 +2,7 @@ package hello.hellospirng.post.service;
 
 import com.amazonaws.services.s3.AmazonS3Client;
 import com.amazonaws.services.s3.model.CannedAccessControlList;
+import com.amazonaws.services.s3.model.DeleteObjectRequest;
 import com.amazonaws.services.s3.model.ObjectMetadata;
 import com.amazonaws.services.s3.model.PutObjectRequest;
 import com.amazonaws.util.IOUtils;
@@ -12,6 +13,8 @@ import org.springframework.web.multipart.MultipartFile;
 
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
 @Slf4j
@@ -30,8 +33,8 @@ public class S3Uploader {
     @Value("${s3.dir}")
     private String dir;  // S3 버킷 이름
 
-    public String  upload( MultipartFile[] files) throws IOException {
-        String returnFilesName = "";
+    public List<String> upload(MultipartFile[] files) throws IOException {
+        List<String> returnFilesName = new ArrayList<>();
 
         for(MultipartFile file : files) {
             String fileName = UUID.randomUUID() + "-" + file.getOriginalFilename();
@@ -43,8 +46,21 @@ public class S3Uploader {
             ByteArrayInputStream byteStream = new ByteArrayInputStream(bytes);
             amazonS3Client.putObject(new PutObjectRequest(bucket, dir + fileName, byteStream, metadata)
                     .withCannedAcl(CannedAccessControlList.PublicRead));
-            returnFilesName = "["+amazonS3Client.getUrl(bucket,dir+fileName)+"]\n";
+            returnFilesName.add(amazonS3Client.getUrl(bucket,dir+fileName)+"");
         }
         return returnFilesName;
+    }
+
+    public boolean delete(String key){
+        try {
+            DeleteObjectRequest deleteObj = new DeleteObjectRequest( bucket , key);
+            amazonS3Client.deleteObject(deleteObj);
+            log.info(String.format("[%s] 삭제 요청 완료", key));
+            return true;
+ 
+        }catch (Exception e ){
+            log.warn(String.format("[%s] 삭제 요청 실패", key));
+            return false;
+        }
     }
 }
